fee3283cbfd3f88ea1f7a27d27d4d05a210d8020
prateek <prateekgupta0822@gmail.com>
2026-06-21 01:29:53 +0530

    LLd lecture 141 - Concurency 1:- Intro to Processes and Threads Lecture 142 -Concurency 2 :- Executors and Callables

--- src/Multithreading/CallableDemo/Main.java
+++ src/Multithreading/CallableDemo/Main.java
@@ -1,7 +1,25 @@
 package Multithreading.CallableDemo;
 
-public class Main {
-    public static void main(String[] args){
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        List<Integer> l = List.of(7,3,1,2,4,6,17,12);
+        //dynamic  thread pool genrate threads automatically (on demand) when required
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        Sorter sorter = new Sorter(l,executorService);
+        //use of future
+        //using future ensures whenever we have output we can store it in future (a kind of box ) meanwhile other
+        // task not related to this can keep processing
+        Future<List<Integer>> output = executorService.submit(sorter);
+       // List<Integer>output = sorter.call();
+        System.out.println("Unrealted work `");
+        System.out.println(output.get());//Blocking code "whenever we use output.get ie future .get it will wait for the futuure's output to come
+        executorService.shutdown();
     }
 }
--- src/Multithreading/CallableDemo/Sorter.java
+++ src/Multithreading/CallableDemo/Sorter.java
@@ -3,36 +3,72 @@ package Multithreading.CallableDemo;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 public class Sorter implements Callable<List<Integer>> {
     private List<Integer> arr;
+    ExecutorService executorService;
 
-    Sorter(List<Integer> arr){
-        this.arr =arr; //shallow copy(array in main and this class will point to same address)
+    Sorter(List<Integer> arr, ExecutorService executorService) {
+        this.arr = arr;
+        this.executorService =executorService;//shallow copy(array in main and this class will point to same address)
     }
 
     @Override
     public List<Integer> call() throws Exception {
         //Business Logic (Merge Sort)
         //base case
-        if(arr.size()<=1){
+        if (arr.size() <= 1) {
             return arr;
         }
         //recursive case
         int n = arr.size();
-        int mid = n/2;
+        int mid = n / 2;
         List<Integer> leftArr = new ArrayList<>();
         List<Integer> rightArr = new ArrayList<>();
 
-        for(int i=0; i<n; i++){
-            if(i<mid){
+        for (int i = 0; i < n; i++) {
+            if (i < mid) {
                 leftArr.add(arr.get(i));
-            }else{
+            } else {
                 rightArr.add(arr.get(i));
             }
         }
 
         //Recurrsively sort 2 array
-        return List.of();
+        Sorter leftSorter = new Sorter(leftArr, executorService);
+        Sorter rightSorter = new Sorter(rightArr, executorService);
+
+        Future<List<Integer>> leftFuture = executorService.submit(leftSorter);
+        Future<List<Integer>> rightFuture = executorService.submit(rightSorter);
+        leftArr = leftFuture.get();
+        rightArr = rightFuture.get();
+
+        //Merge the two sorted arrays
+        List<Integer> output = new ArrayList<>();
+        int j=0; int i =0;
+        while(i<leftArr.size() && j<rightArr.size()){
+            if(leftArr.get(i)<rightArr.get(j)){
+                output.add(leftArr.get(i));
+                i++;
+            }else{
+                output.add(rightArr.get(j));
+                j++;
+            }
+        }
+        //copy the remaining left
+        while(i<leftArr.size()){
+            output.add(leftArr.get(i));
+            i++;
+        }
+        //copy the remaining right
+        while(j<rightArr.size()){
+            output.add(rightArr.get(j));
+            j++;
+        }
+
+
+        return output;
     }
 }
